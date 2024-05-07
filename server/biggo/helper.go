e0ed7fd7a146e9f96b9597c3d68e8e05ad96a343
manuel <manuel@funmula.com>
2024-05-07 17:16:49 +0800

    debugging tools and basic documentation started

--- server/biggo/helper.go
+++ server/biggo/helper.go
@@ -16,9 +16,25 @@ func Trace(params ...interface{}) {
 	var pParams string
 	if len(params) > 0 {
 		if buffer, err := json.Marshal(struct{ Values []interface{} }{Values: params}); err == nil {
-			pParams = string(buffer)
+			pParams = fmt.Sprintf(" Params: %s", string(buffer))
 		}
 	}
 
-	fmt.Fprintf(os.Stdout, "[%s] Call: %s Params: %s\r\n", time.Now(), f.Name(), pParams)
+	fmt.Fprintf(os.Stdout, "=====TRACE===== [%s] Call: %s%s\r\n", time.Now(), f.Name(), pParams)
+}
+
+func SlowTrace(timeout uint8, params ...interface{}) {
+	pc := make([]uintptr, 10)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+
+	var pParams string
+	if len(params) > 0 {
+		if buffer, err := json.Marshal(struct{ Values []interface{} }{Values: params}); err == nil {
+			pParams = fmt.Sprintf(" Params: %s", string(buffer))
+		}
+	}
+
+	fmt.Fprintf(os.Stdout, "===============\r\n=====SLOW1===== [%s] Call: %s%s\r\n===============\r\n", time.Now(), f.Name(), pParams)
+	time.Sleep(time.Second * time.Duration(timeout))
 }
