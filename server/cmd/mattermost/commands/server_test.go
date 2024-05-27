346b578b817c117e23c09f53f26d5061d31dc982
Louis <louis@funmula.com>
2024-05-27 20:17:04 +0800

    update tests

--- server/cmd/mattermost/commands/server_test.go
+++ server/cmd/mattermost/commands/server_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"git.biggo.com/Funmula/mattermost-funmula/server/v8/channels/jobs"
-	"git.biggo.com/Funmula/mattermost-funmula/servernfig"
+	"git.biggo.com/Funmula/mattermost-funmula/server/v8/config"
 )
 
 const (
