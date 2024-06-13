9bbfad3b4833a0eb3fb9a4d05f0246618d478281
manuel <manuel@funmula.com>
2024-06-13 10:20:41 +0800

    use root user for mattermost

--- server/build/entrypoint.sh
+++ server/build/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-chown mattermost:mattermost /mattermost/client/plugins
-chown mattermost:mattermost /mattermost/config
-chown mattermost:mattermost /mattermost/plugins
-
 if [ "${1:0:1}" = '-' ]; then
     set -- mattermost "$@"
 fi
