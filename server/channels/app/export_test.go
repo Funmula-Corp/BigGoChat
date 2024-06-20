7f0be2378b565b12e3ca19e5a5a167c28f93cef5
Louis <louis@funmula.com>
2024-06-20 20:34:15 +0800

    Fix tests caused by add new roles

--- server/channels/app/export_test.go
+++ server/channels/app/export_test.go
@@ -992,7 +992,7 @@ func TestExportSchemes(t *testing.T) {
 		err := th1.App.Srv().Store().System().Save(&model.System{Name: model.MigrationKeyAdvancedPermissionsPhase2, Value: "true"})
 		require.NoError(t, err)
 
-		builtInRoles := 23
+		builtInRoles := 25
 		defaultChannelSchemeRoles := 3
 
 		// Verify the roles count is expected prior to scheme creation.
