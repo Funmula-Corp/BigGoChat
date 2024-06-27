a28fc3ba8fc42202e5ef240de07b348d64339fac
Louis <louis@funmula.com>
2024-06-27 18:50:18 +0800

    move sql migrate scripts.

--- server/channels/db/migrations/postgres/000123_add_verified_permission.down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE channelmembers DROP COLUMN IF EXISTS schemeverified;
-ALTER TABLE schemes DROP COLUMN IF EXISTS defaultteamverifiedrole;
-ALTER TABLE schemes DROP COLUMN IF EXISTS defaultchannelverifiedrole;
-ALTER TABLE teammembers DROP COLUMN IF EXISTS schemeverified;
