4b0ac9b8fbec67cb2338df2543989da87e164aef
Benny <benny@funmula.com>
2024-09-06 13:10:03 +0800

    fix: teams migration script

--- server/channels/db/migrations/postgres/000127_alter_team_add_creatorid.up.sql
+++ server/channels/db/migrations/postgres/000127_alter_team_add_creatorid.up.sql
@@ -6,8 +6,8 @@ BEGIN
     IF (SELECT count(*) FROM teams WHERE creatorid IS NULL) > 0 THEN
         WITH tc AS (
             SELECT u.id, t.email FROM teams AS t LEFT JOIN users AS u ON u.email = t.email WHERE u.id IS NOT NULL
-        ) UPDATE team AS t SET creatorid = tc.id FROM tc WHERE t.email = tc.email;
+        ) UPDATE teams AS t SET creatorid = tc.id FROM tc WHERE t.email = tc.email;
 
-        UPDATE team SET creatorid = '' WHERE creatorid IS NULL;
+        UPDATE teams SET creatorid = '' WHERE creatorid IS NULL;
     END IF;
 END $$;
