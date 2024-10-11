2940f4936644d7362808c67de1f064fd43d316a5
Benny <benny@funmula.com>
2024-10-11 12:00:51 +0800

    refactor: ampq client with supervisor

--- server/public/shared/amqp/amqp.go
+++ server/public/shared/amqp/amqp.go
@@ -2,8 +2,9 @@ package amqp
 
 import (
 	"context"
+	"errors"
+	"net"
 	"runtime"
-	"sync"
 	"time"
 
 	"git.biggo.com/Funmula/mattermost-funmula/server/public/shared/mlog"
@@ -17,7 +18,37 @@ const (
 
 var (
 	numWorker = runtime.NumCPU() * 8
+
+	ErrSwitchNewServer = errors.New("amqp switch to new server")
 )
+
+// connect to server with const timeout, run forever until canceled
+func connect(ctx context.Context, url string) (*amqp.Connection, chan *amqp.Error, error) {
+	currentRetryWaiting := time.Second
+	config := amqp.Config{
+		Locale: "en_US",
+		Dial: func(network, addr string) (net.Conn, error) {
+			return net.DialTimeout(network, addr, amqpTimeOut)
+		},
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, nil, ctx.Err()
+		default:
+			client, err := amqp.DialConfig(url, config)
+			if err != nil {
+				mlog.Error("AMQPClient: cannot connect amqp server", mlog.Err(err))
+				time.Sleep(min(currentRetryWaiting, maxRetryWaiting))
+				currentRetryWaiting *= 2
+				continue
+			}
+			mlog.Info("AMQPClient: remote server connected")
+			return client, client.NotifyClose(make(chan *amqp.Error)), nil
+		}
+	}
+}
 
 type AMQPMessage struct {
 	Exchange string
@@ -29,26 +60,36 @@ type AMQPMessage struct {
 type AMQPClient struct {
 	url    string
 	client *amqp.Connection
-	ctx    context.Context
-	cancel func()
+	ctx    context.Context // root context
+	cancel func()          // root cancel
 	queue  chan AMQPMessage
-	lock   *sync.Mutex
+	err    chan error
 }
 
+// **MUST** call this to create AMQPClient service
 func MakeAMQPClient(url string) *AMQPClient {
+	ctx, cancel := context.WithCancel(context.Background())
 	amqpClient := &AMQPClient{
 		url: url,
 		// buffer already in PushNotificationsHub
-		queue: make(chan AMQPMessage),
-		lock:  new(sync.Mutex),
+		queue:  make(chan AMQPMessage),
+		ctx:    ctx,
+		cancel: cancel,
+		err:    make(chan error),
 	}
-	go amqpClient.reconnect()
+
+	go amqpClient.supervisor()
 
 	return amqpClient
 }
 
-func (a *AMQPClient) Publish(message AMQPMessage) {
-	a.queue <- message
+func (a *AMQPClient) Publish(message AMQPMessage) error {
+	select {
+	case a.queue <- message:
+		return nil
+	case <-a.ctx.Done():
+		return a.ctx.Err()
+	}
 }
 
 // **unsafe** expose channel for other usage
@@ -57,94 +98,116 @@ func (a *AMQPClient) Channel() (*amqp.Channel, error) {
 }
 
 func (a *AMQPClient) Shutdown() {
-	if a.cancel != nil {
-		a.cancel()
-	}
-	if a.client != nil {
-		a.client.Close()
-	}
+	a.cancel()
 }
 
 // connect to new url without drop data
-func (a *AMQPClient) SwitchToNewServer(url string) {
+func (a *AMQPClient) SwitchToNewServer(url string) error {
 	a.url = url
-	go a.reconnect()
+	select {
+	case a.err <- ErrSwitchNewServer:
+	case <-a.ctx.Done():
+		return a.ctx.Err()
+	}
+	return nil
 }
 
-func (a *AMQPClient) reconnect() {
-	// can only run reconnect once at same time
-	if a.lock.TryLock() {
-		defer a.lock.Unlock()
-	} else {
+func (a *AMQPClient) supervisor() {
+	// startup
+	client, notifyClose, err := connect(a.ctx, a.url)
+	if err != nil {
+		// only root cancel can cause err
 		return
 	}
+	a.client = client
 
-	a.Shutdown()
-
-	currentRetryWaiting := time.Second
-
-	// retry connect until amqp connected
-	for {
-		client, err := amqp.Dial(a.url)
-		if err != nil {
-			mlog.Error("AMQPClient: cannot connect amqp server", mlog.Err(err))
-			time.Sleep(min(currentRetryWaiting, maxRetryWaiting))
-			currentRetryWaiting *= 2
-			continue
+	createWorkers := func(ctx context.Context, c int) func() {
+		sub, cancel := context.WithCancel(ctx)
+		for i := 0; i < c; i++ {
+			go a.worker(sub)
 		}
-		a.client = client
-		a.ctx, a.cancel = context.WithCancel(context.Background())
-
-		// start workers
-		for i := 0; i < numWorker; i++ {
-			go a.worker()
-		}
-
-		break
+		return cancel
 	}
-	mlog.Info("AMQPClient: remote server connected")
+
+	cancelWorkers := createWorkers(a.ctx, numWorker)
+
+	// watch
+	for {
+		select {
+		case notify := <-notifyClose:
+			if notify == nil {
+				mlog.Info("AMQPClient: connection closed")
+			} else {
+				mlog.Info("AMQPClient: connection closed", mlog.Err(notify))
+			}
+			cancelWorkers()
+			a.client, notifyClose, err = connect(a.ctx, a.url)
+			if err != nil {
+				// root canceled
+				return
+			}
+			cancelWorkers = createWorkers(a.ctx, numWorker)
+		case err := <-a.err:
+			if errors.Is(err, ErrSwitchNewServer) {
+				a.client.Close()
+			}
+		case <-a.ctx.Done():
+			// full shutdown
+			if a.client != nil {
+				a.client.Close()
+			}
+		}
+	}
 }
 
-// can only call by worker
-func (a *AMQPClient) handleErr(err error) {
-	mlog.Error("AMQPClient: worker failed", mlog.Err(err))
+func (a *AMQPClient) worker(ctx context.Context) {
+	handleErr := func(err error) {
+		mlog.Error("AMQPClient: worker failed", mlog.Err(err))
+		select {
+		case a.err <- err:
+		case <-ctx.Done():
+		}
+	}
 
-	// should always reconnect or just amqp server down?
-	// if errors.Is(err, amqp.ErrClosed) {
-	// }
-	a.reconnect()
-}
-
-func (a *AMQPClient) worker() {
 	channel, err := a.client.Channel()
 	if err != nil {
-		a.handleErr(err)
+		handleErr(err)
 		return
+	}
+	defer channel.Close()
+
+	// enable confirm mode
+	if err := channel.Confirm(false); err != nil {
+		handleErr(err)
 	}
 
 	for {
 		select {
 		case message := <-a.queue:
-			if err := publish(a.ctx, channel, message); err != nil {
-				// put it back
+			ok, err := publish(ctx, channel, message)
+			if !ok || err != nil {
 				go func() { a.queue <- message }()
-				a.handleErr(err)
-				return
 			}
-		case <-a.ctx.Done():
+			if err != nil {
+				handleErr(err)
+			}
+		case <-ctx.Done():
 			return
 		}
 	}
 }
 
-func publish(parent context.Context, channel *amqp.Channel, message AMQPMessage) error {
-	ctx, cancel := context.WithTimeout(parent, amqpTimeOut)
-	defer cancel()
-
-	err := channel.PublishWithContext(ctx, message.Exchange, message.Key, false, false, amqp.Publishing{
+func publish(ctx context.Context, channel *amqp.Channel, message AMQPMessage) (bool, error) {
+	confirm, err := channel.PublishWithDeferredConfirm(message.Exchange, message.Key, false, false, amqp.Publishing{
 		Headers: message.Headers,
 		Body:    message.Body,
 	})
+	if err != nil {
+		return false, err
+	}
 
-	return err
+	sub, cancel := context.WithTimeout(ctx, amqpTimeOut)
+	defer cancel()
+
+	return confirm.WaitContext(sub)
 }
