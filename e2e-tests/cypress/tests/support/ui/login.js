58f57a334bfde2ea07daa5572e02842e8ab5026c
Louis <louis@funmula.com>
2024-11-14 19:09:32 +0800

    E2E: a workaround to login

--- e2e-tests/cypress/tests/support/ui/login.js
+++ e2e-tests/cypress/tests/support/ui/login.js
@@ -2,10 +2,12 @@
 // See LICENSE.txt for license information.
 
 Cypress.Commands.add('uiLogin', (user = {}) => {
-    cy.url().should('include', '/login');
+    // BigGo dosen't have password login, so we use apiLogin to login
+    cy.apiLogin(user).reload()
 
-    // # Type email and password, then Sign in
-    cy.get('#input_loginId').should('be.visible').type(user.email);
-    cy.get('#input_password-input').should('be.visible').type(user.password);
-    cy.get('#saveSetting').should('not.be.disabled').click();
+    // cy.url().should('include', '/login');
+    // // # Type email and password, then Sign in
+    // cy.get('#input_loginId').should('be.visible').type(user.email);
+    // cy.get('#input_password-input').should('be.visible').type(user.password);
+    // cy.get('#saveSetting').should('not.be.disabled').click();
 });
